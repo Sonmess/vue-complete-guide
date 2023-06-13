11b930e8d2541e66e4fbcea1f12d053476cc2f4d
bohac <matej.bohac@ringieraxelspringer.sk>
2023-06-13 16:26:15 +0200

    ~ dokoncenie my goals app, nova evenets app

--- section-2-my-goals-app/app.js
+++ section-2-my-goals-app/app.js
@@ -2,7 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoal: "Finish the course and learn Vue.js",
+      courseGoals: [
+        'Finish the course and learn Vue.js',
+        'Master the course and learn Vue.js'
+      ],
+      vueLink: 'https://vuejs.org/'
     };
   },
+  methods: {
+    outputGoal() {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5) {
+            return this.courseGoals[0];
+        } else {
+            return this.courseGoals[1];
+        }
+    }
+  }
 });
 app.mount("#user-goal");
