8622b24ee591ecaa00a1d1544f7a1768063af155
bohac <matej.bohac@ringieraxelspringer.sk>
2023-06-15 09:42:51 +0200

    ~ dokoncenie dalsej lekcie

--- section-2-events-app/app.js
+++ section-2-events-app/app.js
@@ -2,8 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+  methods: {
+    addCounter(number = 1) {
+      this.counter = this.counter + number;
+    },
+    substractCounter(number = 1) {
+      this.counter = this.counter - number;
+    },
+    setName(e, lastName) {
+      this.name = e.currentTarget.value + " " + lastName;
+    },
+    submitForm() {
+      alert("Submitted");
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
