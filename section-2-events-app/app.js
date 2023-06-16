f9c8b0efc262385aa70f80f44bc4c3fb4ca11c66
bohac <matej.bohac@ringieraxelspringer.sk>
2023-06-16 10:42:08 +0200

    ~ dokoncenie druhej lekcie

--- section-2-events-app/app.js
+++ section-2-events-app/app.js
@@ -3,7 +3,25 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "Bohac",
+      anotherName: ''
     };
+  },
+  watch: {
+    anotherName(newValue, oldValue) {
+      if (newValue === 'Matej') {
+        alert('Vitaj ' + newValue);
+      }      
+    }
+  },
+  computed: {
+    fullName() {
+      console.log('Running again ...');
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
   },
   methods: {
     addCounter(number = 1) {
@@ -12,11 +30,14 @@ const app = Vue.createApp({
     substractCounter(number = 1) {
       this.counter = this.counter - number;
     },
-    setName(e, lastName) {
-      this.name = e.currentTarget.value + " " + lastName;
+    setName(e) {
+      this.name = e.currentTarget.value;
     },
     submitForm() {
       alert("Submitted");
+    },
+    resetInput() {
+      this.name = "";
     },
   },
 });
