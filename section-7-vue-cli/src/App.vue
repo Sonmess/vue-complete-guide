c57cbf8340c7ace17a03df63540b49d353983f48
bohac <matej.bohac@ringieraxelspringer.sk>
2023-07-07 11:31:13 +0200

    ~ dokoncenie 7 sekcie vue-cli

---
+++ section-7-vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="container">
+        <h2>My friends</h2>
+        <ul>
+            <FriendContact v-for="item of friends" :key="item.id" :friend="item" />
+        </ul>
+    </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+
+export default {
+    components: {FriendContact},
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'matej',
+                    name: 'Matej Bohac',
+                    phone: '0123 456 78 90',
+                    email: 'matej@localhost.com'
+                },
+                {
+                    id: 'luca',
+                    name: 'Lucia Sliacka',
+                    phone: '0123 456 78 90',
+                    email: 'lucia@localhost.com'
+
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ section-7-vue-cli/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <div class="details" v-if="detailsVisible">
+            <div>{{ friend.phone }}</div>
+            <div>{{ friend.email }}</div>
+        </div>        
+        <button @click="toggleDetails">Show / Hide</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        friend: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            detailsVisible: true
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsVisible = !this.detailsVisible;
+        }
+    }
+}
+</script>
