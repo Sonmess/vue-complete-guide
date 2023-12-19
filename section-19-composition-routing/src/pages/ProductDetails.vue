0707d5c2efeed0f7842605d4b207a360328d3911
bohac <matej.bohac@ringier.sk>
2023-12-19 15:58:40 +0100

    ~ section 19 composition routing

---
+++ section-19-composition-routing/src/pages/ProductDetails.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <h3>${{ price }}</h3>
+    <p>{{ description }}</p>
+    <router-link to="/product/p2">Product 2</router-link>
+  </section>
+</template>
+
+<script>
+import { inject, computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default {
+  props: ['pid'],
+  setup(props) {
+    const products = inject('products');
+
+    const selectedProduct = computed(() =>
+      products.value.find((product) => product.id === props.pid)
+    );
+    const title = selectedProduct.value.title;
+    const price = selectedProduct.value.price;
+    const description = selectedProduct.value.description;
+
+    const route = useRoute();
+    console.log(route);
+
+    return { title, price, description };
+  },
+};
+</script>
+
+<style scoped>
+section {
+  margin: 3rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
