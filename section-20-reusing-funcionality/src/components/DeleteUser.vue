b7be06de20aa047414fccf85a610fce53d53313d
bohac <matej.bohac@ringier.sk>
2024-01-30 10:21:50 +0100

    ~ section 20 finished

---
+++ section-20-reusing-funcionality/src/components/DeleteUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script>
+import alertMixin from '../mixins/alert';
+import UserAlert from './UserAlert.vue';
+
+export default {
+  components: {UserAlert},
+  mixins: [alertMixin],
+  data() {
+    return {
+      alertTitle: 'Delete the User?',
+      alertIsVisible: true
+    }
+  }
+};
+</script>
---
+++ section-20-reusing-funcionality/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import Logger from './mixins/logger';
+
+const app = createApp(App);
+app.mixin(Logger);
+
+app.mount('#app');
